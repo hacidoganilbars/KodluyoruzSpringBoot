8a1313785bfc82c2ea08a4083f6faa442b5b4007
Haci Dogan ilbars <doganilbars80@gmail.com>
2022-05-12 20:48:20 +0300

    PostConstuct

---
+++ src/main/java/com/doganilbars/controller/PostConstructTutorialController.java
@@ -0,0 +1,32 @@
+package com.doganilbars.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.annotation.PostConstruct;
+import java.util.logging.Logger;
+
+@Controller
+public class PostConstructTutorialController {
+
+    @Autowired
+    Logger LOG;
+
+    //parametresiz constructor
+    // because "this.LOG" is null
+  /*  public PostConstructTutorialController() {
+        LOG.info("Log info çağrıldı");
+    }*/
+
+    //bean henüz başlamadığından null  alıyoruz ve burada bağımlılığı enjecte edemiyoruz.
+    //Görevi: Bir bean nesnesi oluşturulduğunda hemen oluşur.
+    @PostConstruct
+    public void init(){
+        LOG.info("Log info çağırıldı");
+    }
+
+    public static void main(String[] args) {
+        PostConstructTutorialController postConstructTutorialController = new PostConstructTutorialController();
+        System.out.println(postConstructTutorialController);
+    }
+}
