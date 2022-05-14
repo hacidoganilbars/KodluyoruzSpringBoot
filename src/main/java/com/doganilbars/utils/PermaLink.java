798e131b129a121b42d9f87f404e8774a6fd1ca7
Haci Dogan ilbars <doganilbars80@gmail.com>
2022-05-14 23:24:51 +0300

    PermaLink revize

--- src/main/java/com/doganilbars/utils/PermaLink.java
+++ src/main/java/com/doganilbars/utils/PermaLink.java
@@ -2,5 +2,9 @@ package com.doganilbars.utils;
 
 public class PermaLink {
 
-    //Github: https://github.com/hacidoganilbars/KodluyoruzSpringBoot
+    //  Github: https://github.com/hacidoganilbars/KodluyoruzSpringBoot
+    //  SWAGGER: http://localhost:8080/swagger-ui/index.html
+    //  H2-console: http://localhost:8080/h2-console/
+    //  JSON Online parser:  https://jsoneditoronline.org/#left=local.gesomi&right=local.revibe
+    //  Jwt ==> https://jwt.io/
 }
