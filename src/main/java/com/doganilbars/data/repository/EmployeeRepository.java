0a36190296b3b47ffff09b5ea9d34780eb116204
Haci Dogan ilbars <doganilbars80@gmail.com>
2022-05-14 19:30:04 +0300

    EmployeRepository -> JpaRepository(@Repository)

--- src/main/java/com/doganilbars/data/repository/EmployeeRepository.java
+++ src/main/java/com/doganilbars/data/repository/EmployeeRepository.java
@@ -1,8 +1,10 @@
 package com.doganilbars.data.repository;
 
+import com.doganilbars.data.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends CrudRepository {
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity,Long> {
 }
